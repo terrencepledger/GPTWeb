522a306fcba71a9f12e82f31f4a44ea63bd6b207
terrencepledger <terrencepledger@gmail.com>
2025-08-18 18:04:57 -0500

    feat: add brand and neutral colors

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          purple: "#5C30A6",
+          purpleLt: "#B19CD9",
+          gold: "#D6AF36",
+          ink: "#121212",
+        },
+        neutral: {
+          50: "#F0F0F0",
+          100: "#E0E0E0",
+          200: "#D0D0D0",
+          300: "#C0C0C0",
+          400: "#B0B0B0",
+          500: "#A0A0A0",
+          600: "#909090",
+          700: "#808080",
+          800: "#707070",
+          900: "#606060",
+          950: "#404040",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
