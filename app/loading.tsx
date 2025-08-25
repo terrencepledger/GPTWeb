696a9e220174ae81128aec6372c8b2068e0ee7b3
terrencepledger <terrencepledger@gmail.com>
2025-08-25 11:06:06 -0500

    Use SiteSettings logo for loader

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import LogoSpinner from "@/components/LogoSpinner";
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <LogoSpinner />
+    </div>
+  );
+}
